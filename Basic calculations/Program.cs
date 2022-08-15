d0aed1c8effa68d8f004976f2b99193506409ab3
Livhuwani <Livhuwani@LAPTOP-QTRB2ACR>
2022-08-15 19:13:46 +0200

    Add project files.

---
+++ Basic calculations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1, value2;
+            
+            Console.WriteLine("Basic Mathematical calculations");
+
+            Console.WriteLine("Enter your first Value:");
+            value1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your second value: ");
+            value2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Addition: " + (value1 + value2));
+            Console.WriteLine("Subtraction: " + (value1 - value2));
+            Console.WriteLine("Multiplication: " + value1 * value2);
+            Console.WriteLine("Division: " + (value1/value2));
+
+            Console.ReadLine();
+        }
+    }
+}
